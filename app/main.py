dd649d098508fac8d4505f6c40b428467112c31d
thahir2005 <skthahir2005@gmail.com>
2026-01-01 21:49:44 +0530

    feat: postgres connected and users table created (day 4)

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from app.core.config import settings
+from app.core.database import engine, Base
+from app.models import user
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title=settings.APP_NAME)
 
 @app.get("/")
 def root():
-    return {
-        "app": settings.APP_NAME,
-        "debug": settings.DEBUG
-    }
+    return {"db": "connected"}
+
